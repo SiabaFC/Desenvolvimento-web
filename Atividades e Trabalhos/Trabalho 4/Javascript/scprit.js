48890ab202f6612a0b938a87dc91e565ecac03ce
Felipe Siaba <160870389+SiabaFC@users.noreply.github.com>
2024-03-25 11:10:08 -0300

    Add files via upload

---
+++ Atividades e Trabalhos/Trabalho 4/Javascript/scprit.js
@@ -0,0 +1,17 @@
+console.log('começo')
+
+let lista = [1,2,3,4];
+
+function minhafun(par){
+    return 'não faz nada' + par
+}
+
+console,log('no meio');
+
+lista,push('novo');
+
+console.log(lista,leight)
+
+console.log(minhafun('ou tudo'))
+
+console.log('terminou')
